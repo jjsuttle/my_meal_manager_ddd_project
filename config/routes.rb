d5b49e69df05e21147f61810b7a953deb810f61f
anonymous anonymous <john.suttle@kellogg.northwestern.edu>
2021-12-08 21:37:12 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "inventory_of_food_items#index"
   resources :recipe_reviews
   resources :recipes
