60448701e21c07600543c2b81cdf96d54dc7a9af
anonymous anonymous <john.suttle@kellogg.northwestern.edu>
2021-12-08 21:37:27 +0000

    Add foreign key food_id reference to recipe_ingredients table

---
+++ db/migrate/20211208213727_add_food_reference_to_recipe_ingredients.rb
@@ -0,0 +1,6 @@
+class AddFoodReferenceToRecipeIngredients < ActiveRecord::Migration[6.0]
+  def change
+    add_foreign_key :recipe_ingredients, :inventory_of_food_items, column: :food_id
+    add_index :recipe_ingredients, :food_id
+  end
+end
