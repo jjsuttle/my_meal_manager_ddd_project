3ba3f4bab6521f52e4d5702cb060e56c035cdc2b
anonymous anonymous <john.suttle@kellogg.northwestern.edu>
2021-12-08 21:37:17 +0000

    Whitelist columns in RecipeIngredient for ActiveAdmin

--- app/admin/recipe_ingredients.rb
+++ app/admin/recipe_ingredients.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register RecipeIngredient do
 
+ permit_params :recipe_id, :food_id, :ingredient_name, :quantity, :quantity_units
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
