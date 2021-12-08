37729fba1f588851eee1abdd4077ac84ac8a7c3c
anonymous anonymous <john.suttle@kellogg.northwestern.edu>
2021-12-08 21:37:54 +0000

    Generate graphiti resources for inventory_of_food_items

---
+++ app/controllers/api/v1/inventory_of_food_items_controller.rb
@@ -0,0 +1,41 @@
+class Api::V1::InventoryOfFoodItemsController < Api::V1::GraphitiController
+  def index
+    inventory_of_food_items = InventoryOfFoodItemResource.all(params)
+    respond_with(inventory_of_food_items)
+  end
+
+  def show
+    inventory_of_food_item = InventoryOfFoodItemResource.find(params)
+    respond_with(inventory_of_food_item)
+  end
+
+  def create
+    inventory_of_food_item = InventoryOfFoodItemResource.build(params)
+
+    if inventory_of_food_item.save
+      render jsonapi: inventory_of_food_item, status: 201
+    else
+      render jsonapi_errors: inventory_of_food_item
+    end
+  end
+
+  def update
+    inventory_of_food_item = InventoryOfFoodItemResource.find(params)
+
+    if inventory_of_food_item.update_attributes
+      render jsonapi: inventory_of_food_item
+    else
+      render jsonapi_errors: inventory_of_food_item
+    end
+  end
+
+  def destroy
+    inventory_of_food_item = InventoryOfFoodItemResource.find(params)
+
+    if inventory_of_food_item.destroy
+      render jsonapi: { meta: {} }, status: 200
+    else
+      render jsonapi_errors: inventory_of_food_item
+    end
+  end
+end
---
+++ app/resources/inventory_of_food_item_resource.rb
@@ -0,0 +1,17 @@
+class InventoryOfFoodItemResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :user_id, :integer
+  attribute :date_purchased, :date
+  attribute :expiration_date, :date
+  attribute :days_until_expiration, :integer
+  attribute :purchased_status, :string_enum, allow: InventoryOfFoodItem.purchased_statuses.keys
+  attribute :food_name, :string
+  attribute :notes, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
